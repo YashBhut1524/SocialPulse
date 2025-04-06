3ffb84c1577682143c9582c5957c5181817a5c7d
YashBhut1524 <yashxerox15@gmail.com>
2025-04-06 11:02:45 +0530

    fix paramsprops for profile

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,8 +7,10 @@ import {
 import ProfilePageClient from "@/components/ProfilePageClient";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
+export async function generateMetadata({ params }: { params: Promise<{ username: string }> }) {
+
+    const username = (await params).username
+    const user = await getProfileByUsername(username);
     if (!user) return;
 
     return {
@@ -17,8 +19,10 @@ export async function generateMetadata({ params }: { params: { username: string 
     };
 }
 
-async function ProfilePageServer({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
+async function ProfilePageServer({ params }: { params: Promise<{ username: string }> }) {
+
+    const username = (await params).username
+    const user = await getProfileByUsername(username);
 
     if (!user) notFound();
 
